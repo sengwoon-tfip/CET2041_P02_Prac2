3163d0b7d2234e63e771def531bc7dd3219fa61e
sengwoon <sengwoon977@gmail.com>
2025-12-02 16:25:14 +0800

    added java docs for Department class

--- src/main/java/jpa_demo_01/entity/Department.java
+++ src/main/java/jpa_demo_01/entity/Department.java
@@ -2,24 +2,55 @@ package jpa_demo_01.entity;
 
 import jakarta.persistence.*;
 
+/**
+ * Entity representing a department in the departments table.
+ * Each department consists of department number and a department name.
+ */
 @Entity
 @Table(name = "departments")
 public class Department {
-
+    /**
+     * Department number associated with the record.
+     */
     @Id
     @Column(name = "dept_no", columnDefinition = "char(4)")
     private String deptNo;
 
+    /**
+     * Department name associated with the record.
+     */
     @Column(name = "dept_name", nullable = false, length = 40)
     private String deptName;
 
+    /**
+     * Default constructor
+     */
     public Department() {
     }
 
+    /**
+     * @return the department number
+     */
     public String getDeptNo() { return deptNo; }
+
+    /**
+     * Sets department number.
+     *
+     * @param deptNo new department number
+     */
     public void setDeptNo(String deptNo) { this.deptNo = deptNo; }
 
+    /**
+     * @return the department name
+     */
     public String getDeptName() { return deptName; }
+
+    /**
+     * Sets department name.
+     *
+     * @param deptName new department name
+     */
     public void setDeptName(String deptName) { this.deptName = deptName; }
 }
+
 
